fb6326b223e2281c125c78adf9c7917feb50d11c
Иван Орищенко <44241496+ifest1val@users.noreply.github.com>
2022-04-27 00:10:29 +0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -4,9 +4,9 @@ sudo apt update -y
 sudo apt install --upgrade git -y
 sudo apt install --upgrade python3 -y
 sudo apt install --upgrade python3-pip -y
-git clone https://github.com/ifest1val/mhd_proxy.git
-cd mhd_proxy/mhddos_proxy-main/
+git clone https://github.com/ifest1val/mhddos_proxy.git
+cd mhddos_proxy/
 ulimit -n 1048576
 sudo python3 -m pip install -r requirements.txt
-cd mhd_proxy/mhddos_proxy-main/
+cd mhddos_proxy/
 screen -S "run" sudo  python3 runner.py -c https://raw.githubusercontent.com/ifest1val/itarmy/main/address/pack_1 -t 1500 --rpc 2000 --http-methods GET STRESS --debug --vpn
