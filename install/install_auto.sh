be5e99776d95bd762d60ee68b8019b1dfca612c0
Иван Орищенко <44241496+ifest1val@users.noreply.github.com>
2022-04-27 10:35:17 +0300

    Update install_auto.sh

--- install/install_auto.sh
+++ install/install_auto.sh
@@ -3,13 +3,10 @@ sudo pkill -f runner.sh
 sudo pkill -f runner.py
 sudo pkill -f ./start.py
 
-#Remove old versions
-sudo rm -R ~/mhddos_proxy
-sudo rm -R ~/auto_mhddos_alexnest
-
-
 sudo apt update -y
 sudo apt upgrade -y
+
+
 # Install git, python3
 sudo apt install git gcc libc-dev libffi-dev libssl-dev python3-dev rustc -y
 sudo apt install git -y
@@ -30,6 +27,7 @@ ulimit -n 1048576
 #Install latest version of mhddos_proxy and MHDDoS
 cd ~
 sudo rm -r mhddos_proxy
+sudo rm -R auto_mhddos_alexnest
 git clone https://github.com/ifest1val/auto_mhddos_alexnest.git
 git clone https://github.com/ifest1val/mhddos_proxy.git
 cd ~/mhddos_proxy
