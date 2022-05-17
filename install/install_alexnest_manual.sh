07598a1e024533011a9dc10618d802fa4f4ef208
Ivan Oryshchenko <44241496+ifest1val@users.noreply.github.com>
2022-05-17 21:28:05 +0300

    Update install_alexnest_manual.sh

--- install/install_alexnest_manual.sh
+++ install/install_alexnest_manual.sh
@@ -1,8 +1,9 @@
-cd ~  
+cd ~
 sudo rm -rf auto_mhddos_alexnest
-sudo apt install git -y  
+sudo apt install git -y
 git clone https://github.com/ifest1val/auto_mhddos_alexnest.git
 cd ~/auto_mhddos_alexnest
 chmod +x setup.sh
 bash setup.sh
 screen -S "runner" bash runner.sh
+1
