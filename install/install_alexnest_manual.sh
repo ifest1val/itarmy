19c5672787c8512161c39bcfbf6dd7cc820e47f1
Ivan Oryshchenko <44241496+ifest1val@users.noreply.github.com>
2022-06-23 19:00:45 +0300

    Update install_alexnest_manual.sh

--- install/install_alexnest_manual.sh
+++ install/install_alexnest_manual.sh
@@ -1,7 +1,7 @@
 cd ~
 sudo rm -rf auto_mhddos_alexnest
 sudo apt install git -y
-git clone https://github.com/ifest1val/auto_mhddos_alexnest.git
+git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
 cd ~/auto_mhddos_alexnest
 chmod +x setup.sh
 bash setup.sh
