7bd99845410405ab576b5ed22c425bfad2cfd161
Ivan Oryshchenko <44241496+ifest1val@users.noreply.github.com>
2022-06-25 11:40:16 +0300

    Update stp.sh

--- install/stp.sh
+++ install/stp.sh
@@ -1,6 +1,6 @@
 cd ~  
 sudo rm -rf auto_mhddos_alexnest
 sudo apt install git screen -y  
-sudo git clone https://github.com/alexnest-ua/auto_mhddos_alexnest
+sudo git clone https://github.com/ifest1val/auto_mhddos_alexnest
 cd ~/auto_mhddos_alexnest
 screen -S "runner" bash runner.sh  
